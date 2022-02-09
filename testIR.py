94ac9e7330cb286cac3aef7916c73cef5cc1a692
brandonberry4 <brandon.berry.17@cnu.edu>
2022-02-08 21:45:21 -0500

    IR code and picture

--- testIR.py
+++ testIR.py
@@ -3,12 +3,45 @@ GPIO.setwarnings(False)
 GPIO.setmode (GPIO.BCM)
 import time
 
-GPIO.setup(0, GPIO.IN) #GPIO 0 right IR
-GPIO.setup(2, GPIO.IN) #GPIO 2 left IR
+GPIO.setup(36, GPIO.IN) #GPIO 36 right IR
+GPIO.setup(32, GPIO.IN) #GPIO 32 left IR
 
 while True:
-    if (GPIO.input(0) == True and GPIO.input(2) == True): #both see white
-        print("Checking")
-        print("On track")
-        time.sleep(1)
+    if (GPIO.input(32) == False and GPIO.input(36) == False): #both see black
+         print("On track")
+         time.sleep(0.5)
+    elif (GPIO.input(32) == False and GPIO.input(36) == True): #right sees white
+         print("Off track, turning right")
+         time.sleep(0.5)
+    elif (GPIO.input(32) == True and GPIO.input(36) == False): #left sees white
+         print("Off track, turning left")
+         time.sleep(0.5)
+    elif (GPIO.input(32) == True and GPIO.input(36) == False): #right sees white
+         print("Off track, turning right")
+         time.sleep(0.5)
+    elif (GPIO.input(32) == True and GPIO.input(36) == True): #both see white
+         print("End of track stop")
+         time.sleep(0.5)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     if (GPIO.input(32)) == False:
+#         print("On track")
+#         time.sleep(1)
+#     elif (GPIO.input(32)) == True:
+#         print("Off track")
+#         time.sleep(1)
