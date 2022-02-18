1c18ee6785ddad988651ba55ba12f7bc8d9ce87b
brandonberry4 <brandon.berry.17@cnu.edu>
2022-02-18 11:33:56 -0500

    IR working

--- testIR.py
+++ testIR.py
@@ -1,28 +1,6 @@
-#import RPi.GPIO as GPIO
-# GPIO.setwarnings(False)
-# GPIO.setmode (GPIO.BCM)
 import time
 import gpiozero
 
-# GPIO.setup(35, GPIO.IN) #GPIO 35 right IR
-# GPIO.setup(37, GPIO.IN) #GPIO 37 left IR
-
-# while True:
-#     if (GPIO.input(35) == False and GPIO.input(37) == False): #both see black
-#         print("On track")
-#         time.sleep(0.5)
-#         continue
-#     elif (GPIO.input(37) == False and GPIO.input(35) == True): #turn right
-#         print("Off track, turning right")
-#         time.sleep(0.5)
-#         continue
-#     elif (GPIO.input(37) == True and GPIO.input(35) == False): #turn left
-#         print("Off track, turning left")
-#         time.sleep(0.5)
-#         continue
-#     else: # stop
-#         print("End of track, stopping")
-#         time.sleep(0.5)
 
 line_sensorRight = gpiozero.DigitalInputDevice(19)
 line_sensorLeft = gpiozero.DigitalInputDevice(26)
