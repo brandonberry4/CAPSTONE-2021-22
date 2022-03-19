3524dbed02487246acea57de2b774069ffb5a81c
brandonberry4 <40896336+brandonberry4@users.noreply.github.com>
2022-03-19 12:15:19 -0400

    Update ultrasonic.py

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,63 +1,42 @@
 import RPi.GPIO as GPIO
 import time
 
- 
-#GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
- 
-#set GPIO Pins
-GPIO_TRIGGER = 33
-GPIO_ECHO = 31
-#GPIO direction (IN / OUT)
-print("Setting up")
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
-print("Setup done")
- 
-def distance(): 
-    # set Trigger to HIGH
-    GPIO.output(GPIO_TRIGGER, True)
- 
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-    
-    print("Starting time")
-    StartTime = time.time()
-    StopTime = time.time()
- 
-    # save StartTime
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-        print("Start saved")
-        
-    if newReading:
-        return False
- 
-    # save time of arrival
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
-        print("End saved")
- 
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    print("Time Calculated")
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
- 
-    return distance
- 
-if __name__ == '__main__':
-    try:
-        while True:
-            print("Measuring distance")
-            dist = distance()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
- 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print("Measurement stopped by User")
-        GPIO.cleanup()
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    TRIG = 33
+    ECHO = 31
+    maxTime = 0.04
+
+    while True:
+        GPIO.setup(TRIG,GPIO.OUT)
+        GPIO.setup(ECHO,GPIO.IN)
+
+        GPIO.output(TRIG,False)
+
+        time.sleep(0.01)
+
+        GPIO.output(TRIG,True)
+
+        time.sleep(0.00001)
+
+        GPIO.output(TRIG,False)
+
+        pulse_start = time.time()
+        timeout = pulse_start + maxTime
+        while GPIO.input(ECHO) == 0 and pulse_start < timeout:
+            pulse_start = time.time()
+
+        pulse_end = time.time()
+        timeout = pulse_end + maxTime
+        while GPIO.input(ECHO) == 1 and pulse_end < timeout:
+            pulse_end = time.time()
+
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17000
+        distance = round(distance, 2)
+
+        print(distance)
+except:
+    GPIO.cleanup()
